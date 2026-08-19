13453350b30ce87dee97f5353f7509530e7d61b9
ghassan <ahmedghassan939@gmail.com>
2026-08-19 03:49:28 -0700

    chnaged some of commits in html and    change the size of crads button chnaged footer icon

--- index.js
+++
@@ -1,6 +0,0 @@
-// let menuBtn = document.getElementById("menuBtn");
-// let mobileMenu = document.getElementById("mobileMenu");
-
-// menuBtn.addEventListener("click", function () {
-//     mobileMenu.classList.toggle("hidden");
-// });
