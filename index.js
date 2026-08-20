3dd1bdc1c8ff0a84a36f7ae80125290f7b9d3682
ghassan <ahmedghassan939@gmail.com>
2026-08-20 00:44:21 -0700

    changed wrapper size in whole web page added icons in footer section changed the size of projects and  construction type cards

---
+++ index.js
@@ -0,0 +1,8 @@
+   let menuBtn = document.getElementById("menuBtn");
+    let mobilePanel = document.getElementById("mobilePanel");
+
+    menuBtn.addEventListener("click", function(){
+
+        mobilePanel.classList.toggle("hidden");
+
+    });
